f714aa996d5ab3328a642af27915ed01f3674a3c
96sMicks <melkiasae@hotmail.com>
2021-10-12 21:45:17 +0000

    For trial run, was able to get a reoccuring loop of questions going with a while loop

--- game.rb
+++ game.rb
@@ -3,22 +3,30 @@ require './question'
 
 
 class Game
+  is_game_over = false
+  new_player1 = Player.new("Mic")
+  new_player2 = Player.new("Steve")
+  
+  while(!is_game_over)
+    new_question = Question.new
 
- 
-  new_player = Player.new("Mic")
-  new_question = Question.new
+    print "#{new_player1.name}: #{new_question.print_question}"
 
-  print "#{new_player.name}: #{new_question.print_question}"
+    print "> " 
+    answer = gets.chomp.to_i
 
-  print "> " 
-  answer = gets.chomp.to_i
+    if answer == new_question.correct_answer?
+     puts "YES! You are correct"
+    else 
+      puts "Wrong answer"
+      new_player1.lose_life
+    end
 
-  if answer == new_question.correct_answer?
-    puts "YES! You are correct"
-  else 
-    puts "Wrong answer"
-    new_player.lose_life
+    puts "#{new_player1.name} has #{new_player1.lives}/3 lives vs #{new_player2.name} has #{new_player2.lives}/3 lives"
+
+    if new_player1.lives > 0 
+      is_game_over = false
+      puts "--- NEW TURN ---"
+    end
   end
-
-  puts "#{new_player.name} has #{new_player.lives}/3 lives vs #{new_player.name} has #{new_player.lives}/3 lives"
 end
