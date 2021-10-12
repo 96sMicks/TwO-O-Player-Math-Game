310c5b437a8b3bd03b8babf7ce35b85d87d153c4
96sMicks <melkiasae@hotmail.com>
2021-10-12 21:11:19 +0000

    method names changed to reflect functionality

--- question.rb
+++ question.rb
@@ -3,21 +3,15 @@ class Question
   attr_reader :num1, :num2
 
   def initialize
-    @num1 = rand(21)
-    @num2 = rand(21)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   end
 
-  def question
+  def print_question
     puts "What does #{num1} plus #{num2} equal?"
   end
 
-  def correct_answer
+  def correct_answer?
     num1 + num2
   end
 end
-
-q = Question.new
-puts q.num1
-puts q.num2
-puts q.correct_answer
-puts q.question
