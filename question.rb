c336e98615380f3024afeed2b9ab3f2c72c5d5c7
96sMicks <melkiasae@hotmail.com>
2021-10-12 18:47:32 +0000

    the question method to print on console made

--- question.rb
+++ question.rb
@@ -1,10 +1,14 @@
 class Question
 
   attr_reader :num1, :num2
-  
+
   def initialize
     @num1 = rand(21)
     @num2 = rand(21)
+  end
+
+  def question
+    puts "What does #{num1} plus #{num2} equal?"
   end
 
   def correct_answer
@@ -16,3 +20,4 @@ q = Question.new
 puts q.num1
 puts q.num2
 puts q.correct_answer
+puts q.question
