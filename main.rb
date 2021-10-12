54b934d579a78338044a38b5687608c65ad14f37
96sMicks <melkiasae@hotmail.com>
2021-10-12 18:33:24 +0000

    Intial commit, barebones version of classes made for player class

---
+++ main.rb
@@ -0,0 +1,7 @@
+require './players'
+require './question'
+require './game'
+
+# p = Player.new("Mic")
+# puts p.lives
+# puts p.name
---
+++ players.rb
@@ -0,0 +1,11 @@
+class Player
+
+  attr_reader :lives, :name
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+  
+end
+
